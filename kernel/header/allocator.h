b71fdf2343a16b7660d5239c604cfd36f7b64fa6
Deng-Hao, Hsu <72210437+za970120604@users.noreply.github.com>
2025-03-21 23:36:18 +0800

    Delete kernel directory

--- kernel/header/allocator.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALLOCATOR_H
-#define ALLOCATOR_H
-
-#define MAX_SIMPLE_MALLOC_SIZE 16384
-void* task_malloc(unsigned int);
-void* user_malloc(unsigned int );
-
-#endif // ALLOCATOR_H
--- kernel/header/shell.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-void display_help();
-void process_command(char *);
-void shell();
-
-#endif // SHELL_H
